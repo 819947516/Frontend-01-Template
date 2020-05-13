ebeeed4c41d4d750c4d74de4e9a58205ce757a34
LeeJ <819947516@qq.com>
2020-05-13 23:43:07 +0800

    response

--- week05/toy-browser/client.js
+++ week05/toy-browser/client.js
@@ -56,6 +56,141 @@ class Response {
 
 }
 
+class ResponseParser{
+    constructor(){
+        this.WAITING_STATUS_LINE = 0;
+        this.WAITING_STATUS_LINE_END = 1;
+        this.WAITING_HEADER_NAME = 2;
+        this.WAITING_HEADER_SPACE = 3;
+        this.WAITING_HEADER_VALUE = 4;
+        this.WAITING_HEADER_LINE_END = 5;
+        this.WAITING_HEADER_BLOCK_END = 6;
+        this.WAITING_BODY = 7;
+
+        this.current = this.WAITING_STATUS_LINE;
+        this.statusLine = "";
+        this.headers = {};
+        this.headerName = "";
+        this.headerValue = "";
+        this.bodyParser = null;
+    }
+    get isFinished(){
+        return this.bodyParser && this.bodyParser.isFinished;
+    }
+    get response(){
+        this.statusLine.match(/HTTP\/1\.1 (\d+) ([\s\S]+)/);
+        return {
+            statusCode: RegExp.$1,
+            statusText: RegExp.$2,
+            headers: this.headers,
+            body: this.bodyParser.content.join('')
+        }
+    }
+    receive(string){
+        for(let i = 0; i < string.length; i++){
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char){
+        if(this.current === this.WAITING_STATUS_LINE){
+            if(char === '\r'){
+                this.current = this.WAITING_STATUS_LINE_END;
+            }else if(char === '\n'){ //这句需要吗？
+                this.current = this.WAITING_HEADER_NAME; //难道服务器会输出没有status line直接进入headers的情况？
+            }else{
+                this.statusLine += char;
+            }   
+        }else if(this.current === this.WAITING_STATUS_LINE_END){
+            if(char === '\n'){
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        }else if(this.current === this.WAITING_HEADER_NAME){
+            if(char === ":"){
+                this.current = this.WAITING_HEADER_SPACE;
+            }else if(char === "\r"){
+                this.current = this.WAITING_HEADER_BLOCK_END;
+                if(this.headers['Transfer-Encoding'] === "chunked"){
+                    this.bodyParser = new TrunkedResponseParser();
+                }
+            }else{
+                this.headerName += char;
+            }
+        }else if(this.current === this.WAITING_HEADER_SPACE){
+            if(char === " "){
+                this.current = this.WAITING_HEADER_VALUE;
+            }
+        }else if(this.current === this.WAITING_HEADER_VALUE){
+            if(char === "\r"){
+                this.current = this.WAITING_HEADER_LINE_END;
+                this.headers[this.headerName] = this.headerValue; // 存进headers
+                this.headerName = "";
+                this.headerValue = "";
+            }else{
+                this.headerValue += char;
+            }
+        }else if(this.current === this.WAITING_HEADER_LINE_END){
+            if(char === '\n'){
+                this.current = this.WAITING_HEADER_NAME; // 循环进入查找headername状态
+            }
+        }else if(this.current === this.WAITING_HEADER_BLOCK_END){
+            if(char === '\n'){
+                this.current = this.WAITING_BODY;
+            }
+        }else if(this.current === this.WAITING_BODY){
+            this.bodyParser.receiveChar(char);
+        }
+    }
+}
+
+class TrunkedResponseParser{
+    constructor(){
+        this.WAITING_LENGTH = 0;
+        this.WAITING_LENGTH_LINE_END = 1;
+        this.READING_TRUNK = 2;
+        this.WAITING_NEW_LINE = 3;
+        this.WAITING_NEW_LINE_END = 4;
+        this.length = 0;
+        this.content = []; //字符串加号运算性能相对较差，所以用数组
+        this.isFinished = false;
+        this.current = this.WAITING_LENGTH;
+    }
+    receiveChar(char){
+        //console.log(JSON.stringify(char)) //可以打印出\n \r字符
+        if(this.current === this.WAITING_LENGTH){
+            if(char === '\r'){
+                if(this.length === 0){
+                    this.isFinished = true;
+                }
+                this.current = this.WAITING_LENGTH_LINE_END;
+            }else{
+                this.length *= 10;
+                this.length += char.charCodeAt(0) - '0'.charCodeAt(0);
+            }   
+        }else if(this.current === this.WAITING_LENGTH_LINE_END){
+            if(char === '\n'){
+                this.current = this.READING_TRUNK;
+            }
+        }else if(this.current === this.READING_TRUNK){
+            if(this.length !== 0 ){ //去掉最后边的\r\n
+                this.content.push(char);
+                this.length --;
+            }
+            if(this.length === 0){
+                this.current = this.WAITING_NEW_LINE;
+            }
+        }else if(this.current === this.WAITING_NEW_LINE){
+            if(char === '\r'){
+                this.current = this.WAITING_NEW_LINE_END;
+            }
+        }else if(this.current === this.WAITING_NEW_LINE_END){
+            if(char === '\n'){
+                this.current = this.WAITING_LENGTH;
+            }
+        }
+    }
+}
+
+
 void async function() {
     let requset = new Requset({
         method: 'POST',
